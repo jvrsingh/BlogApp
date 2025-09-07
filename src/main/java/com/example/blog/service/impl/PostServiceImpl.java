28155b18f40c06b534dc34b5fc0b7a540c7860d3
Jayveer Singh <jjayveerjs@gmail.com>
2025-09-07 18:12:35 +0530

    change in page sort function when get post

--- src/main/java/com/example/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/example/blog/service/impl/PostServiceImpl.java
@@ -38,7 +38,7 @@ public class PostServiceImpl implements PostService {
         Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
 
         // create  pageable instance
-        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
         Page<Post> posts = postRepository.findAll(pageable);
 
         // get content for page object
